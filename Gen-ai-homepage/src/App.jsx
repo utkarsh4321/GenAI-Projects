6d35d1b76a0f2bf2a842a49b7081d1739252ba9e
utkarsh srivastava <30936607+utkarsh4321@users.noreply.github.com>
2024-01-15 18:35:59 +0000

    added contributors page

--- Gen-ai-homepage/src/App.jsx
+++ Gen-ai-homepage/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Contributors from './components/Contributors'
 
 function App() {
- 
-
   return (
     <>
-    <div className='text-blue-700'>gen_ai</div>
+    <Contributors/>
     </>
   )
 }
